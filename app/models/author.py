23f8c569d5b632d51dcfd928e924c5a381df23b0
Kelsey Steven <kelsey.steven@adadevelopersacademy.org>
2024-03-18 13:42:26 -0700

    Create the one-to-many relationship between Author and Book models and generate the db migration file

--- app/models/author.py
+++ app/models/author.py
@@ -1,9 +1,11 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import List
 from ..db import db
 
 class Author(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     name: Mapped[str]
+    books: Mapped[List["Book"]] = relationship(back_populates="author")
 
     def to_dict(self):
         author_as_dict = {
